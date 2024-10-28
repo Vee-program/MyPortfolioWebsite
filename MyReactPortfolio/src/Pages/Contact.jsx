cf16728e557172f449600f3dc6a366f112f652ac
Vee-program <masilelathembelihle44@gmail.com>
2024-10-28 23:02:45 +0200

    added a contact form page

--- MyReactPortfolio/src/Pages/Contact.jsx
+++ MyReactPortfolio/src/Pages/Contact.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div contact-container>
+      <div className="contact-container">
+        <form>
+          <label>Name</label>
+          <br />
+          <input type="name" />
+          <br />
+          <label>Email</label>
+          <br />
+          <input type="email" />
+          <br />
+          <label>Message</label>
+          <br />
+          <textarea></textarea>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
