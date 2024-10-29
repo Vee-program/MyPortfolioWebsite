ba13a0f58c14a6ec346f06766e12313b8ffc1503
Vee-program <masilelathembelihle44@gmail.com>
2024-10-29 11:14:22 +0200

    added a contact us page and icon images

--- MyReactPortfolio/src/Pages/Contact.jsx
+++ MyReactPortfolio/src/Pages/Contact.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import whatsapp from "..assets/whatsapp.png";
+import instagram from "../assets/instagram.png";
+import email from "../assets/email.png";
 
 const Contact = () => {
   return (
     <div contact-container>
+      <div className="page-title">Contact</div>
+      <h1 className="contact-header">Ready to bring ideas to life ?</h1>
+      <p className="contact-subheader">
+        I'm excited to hear from you and explore how we can work together. Let's
+        chat!
+      </p>
       <div className="contact-container">
         <form>
           <label>Name</label>
@@ -13,10 +22,31 @@ const Contact = () => {
           <br />
           <input type="email" />
           <br />
+          <label>Subject</label>
+          <br />
+          <input type="text" />
+          <br />
           <label>Message</label>
           <br />
           <textarea></textarea>
+          <br />
+          <input type="submit" value="Send Message" />
         </form>
+      </div>
+      <div className="icon-container">
+        <div className="icon">
+          <img src={email} alt="email icon" className="icon/" />
+          <p>Contact Us at:</p>
+          <p>kriatestudios@gmail.com</p>
+        </div>
+        <div className="icon">
+          <img src={whatsapp} alt="whatsapp icon" className="icon/" />
+          <p>Contact us at:</p>
+          <p>079447689</p>
+        </div>
+        <div className="icon">
+          <img src={instagram} alt="instagram icon" className="icon/" />
+        </div>
       </div>
     </div>
   );
